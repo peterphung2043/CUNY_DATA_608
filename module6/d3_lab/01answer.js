dbafd0250657eccfc59b92c6eec39e333b6dbc4b
peterphung2043 <pphung000@citymail.cuny.edu>
2023-04-30 21:43:45 -0400

    Made changes to module 6

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,8 +1,20 @@
 d3.csv('ue_industry.csv', data => {
 
-    // Define your scales and generator here.
+    const xScales = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([20, 1180]);
+
+    const yScales = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
+
+    let lines = d3.line()
+        .x(d => xScales(d.index))
+        .y(d => yScales(d.Agriculture));
 
     d3.select('#answer1')
-        // append more elements here
+        .append('path')
+        .attr('d', lines(data))
+        .attr('stroke', '#2e2928')
 
 });
--- module6/d3_lab/02line.js
+++ module6/d3_lab/02line.js
@@ -1,12 +1,14 @@
 dataset = [
-    {'x': 50, 'y': 100},
-    {'x': 100, 'y': 500},
-    {'x': 500, 'y': 300}
+    { 'x': 50, 'y': 100 },
+    { 'x': 100, 'y': 500 },
+    { 'x': 500, 'y': 300 }
 ]
 
 let line2 = d3.line()
     .x(d => d.x)
     .y(d => d.y)
+
+console.log(line2(dataset));
 
 d3.select('#part2')
     .append('path')
